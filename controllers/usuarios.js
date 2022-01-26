80aa8f3d2f47a3685cc32082d1eef5df490e5148
zxkronos <zxkronos@gmail.com>
2022-01-26 20:52:36 -0300

    primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,88 @@
+const { response, request } = require('express');
+const axios = require('axios');
+
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page, 
+        limit
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - usuariosPost',
+        nombre, 
+        edad
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - usuariosPut',
+        id
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - usuariosPatch'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - usuariosDelete'
+    });
+}
+
+const articuloml =  async (req, res = response) => {
+
+
+
+    console.log(req.body.mlc);
+    mlc = req.body.mlc;
+    let getItem = 'https://api.mercadolibre.com/items/'
+    // const config = {
+    //     headers: { Authorization: `Bearer ${tokens[0].token}` }
+    // };
+
+    testMouseM170Catalogo = 'MLC605369288'
+    testMouseM170Normal = 'MLC487543221'
+    testMlc = testMouseM170Normal;
+    getItem += mlc+'?include_attributes=all' //item con variaciones
+     
+    const resp_item = await axios.get(getItem);
+
+    // console.log(resp_item.data);
+    res.json({
+        'id': resp_item.data.id,
+        'titulo': resp_item.data.title,
+        'thumbnail': resp_item.data.thumbnail
+    });
+}
+
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete,
+    articuloml
+}
